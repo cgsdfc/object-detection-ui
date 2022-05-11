1d7f653395fad51eebc47215de7d34893b392d15
cgsdfc <cgsdfc@126.com>
2022-05-11 20:48:43 +0800

    配置面板的回调 完成

--- main.py
+++ main.py
@@ -1,53 +1,117 @@
 from Ui_develop import *
 import sys
-from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from PyQt5.QtWidgets import QMessageBox, QFileDialog, QPushButton, QLineEdit
+import os
+from pathlib import Path
+
+CODES_DIR = "/home/liao/codes"
 
 
-# class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.setWindowTitle('小样本遥感图像目标检测')
+        self.setWindowTitle("小样本遥感图像目标检测")
 
         # 配置文件的callback
+
         ## 基本配置
-        self.ui.pb_project_path.clicked.connect(
-            self.pb_project_path_fun
+        self.filepath_setter(
+            self.ui.pb_project_path,
+            self.ui.le_project_path,
+            isdir=1,
+            directory=CODES_DIR,
         )
-        return
-        
-        self.ui.pb_data_path.clicked.connect(
-            self.pb_data_path_fun
+
+        self.filepath_setter(
+            self.ui.pb_data_path,
+            self.ui.le_data_path,
+            isdir=1,
+            directory=self.dataset_dir(),
         )
+
         ## 架构配置
-        self.ui.pb_backbone_config.clicked.connect(
-            self.pb_backbone_config_fun
+        self.filepath_setter(
+            self.ui.pb_backbone_config,
+            self.ui.le_backbone_config,
+            directory=self.configure_dir(),
         )
-        self.ui.pb_reweight_config.clicked.connect(
-            self.pb_reweight_config_fun
+        self.filepath_setter(
+            self.ui.pb_reweight_config,
+            self.ui.le_reweight_config,
+            directory=self.configure_dir(),
         )
         ## 预训练配置
-        self.ui.pb_data_config_pretrain.clicked.connect(
-            self.pb_data_config_pretrain_fun
+        self.filepath_setter(
+            self.ui.pb_data_config_pretrain,
+            self.ui.le_data_config_pretrain,
+            directory=self.configure_dir(),
         )
-        self.ui.pb_model_path_pretrain.clicked.connect(
-            self.pb_model_path_pretrain_fun
+        self.filepath_setter(
+            self.ui.pb_model_path_pretrain,
+            self.ui.le_model_path_pretrain,
+            directory=self.model_dir(),
         )
         ## 小样本微调配置
-        self.ui.pb_data_config_fewtune.clicked.connect(
-            self.pb_data_config_fewtune_fun
+        self.filepath_setter(
+            self.ui.pb_data_config_fewtune,
+            self.ui.le_data_config_fewtune,
+            directory=self.configure_dir(),
         )
-        self.ui.pb_model_path_fewtune.clicked.connect(
-            self.pb_model_path_fewtune_fun
+        self.filepath_setter(
+            self.ui.pb_model_path_fewtune,
+            self.ui.le_model_path_fewtune,
+            directory=self.model_dir(),
         )
 
+    def project_dir(self):
+        value = self.ui.le_project_path.text()
+        if value and os.path.isdir(value):
+            return value
+        return None
 
-    def pb_project_path_fun(self):
-        pass
+    def configure_dir(self):
+        prj = self.project_dir()
+        if prj is None:
+            return None
+        return os.path.join(prj, "cfg")
 
-if __name__ == '__main__':
+    def dataset_dir(self):
+        prj = self.project_dir()
+        if prj is None:
+            return None
+        return os.path.join(prj, "dataset")
+
+    def model_dir(self):
+        prj = self.project_dir()
+        if prj is None:
+            return None
+        return os.path.join(prj, "backup")
+
+    def filepath_setter(
+        self, button: QPushButton, lineedit: QLineEdit, isdir=0, directory=None,
+    ):
+        def slot():
+            if isdir:
+                value = QFileDialog.getExistingDirectory(
+                    self, "输入目录路径", directory=directory
+                )
+            else:
+                value, _ = QFileDialog.getOpenFileName(
+                    self, "输入文件路径", directory=directory
+                )
+
+            if not value:
+                print(f"用户没有任何输入：{value}")
+                return
+            lineedit.setText(value)
+            print(f"路径设置成功：{value}")
+
+        button.clicked.connect(slot)
+
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     w = MyWindow()
     w.show()
