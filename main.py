a4aee19db4f22b7e4dde4821bcff0b2283da019f
cgsdfc <cgsdfc@126.com>
2022-05-11 17:06:41 +0800

    不要继承，没有提示了

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from Ui_develop import *
 import sys
 
 
-class MyWindow(QtWidgets.QMainWindow, Ui_MainWindowv):
+# class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-
-        self.setupUi(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle('小样本遥感图像目标检测')
+        
+        
 
 
 if __name__ == '__main__':
